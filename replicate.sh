21575dc2179d6e4edf316702e1a9fa39bd9b121f
Will <willtemperley@gmail.com>
2016-05-17 15:26:43 +0000

    can update a country o5m file with the correct minutely diff

--- replicate.sh
+++ replicate.sh
@@ -1,19 +1,54 @@
 #!/bin/bash
 
-# TODO pull in csv list of countries (from e.g. crontab)
+# arguments are a list of country iso3 codes
+# useful in e.g. a crontab which asks to process several countries
 
-# Convert to o5m
-osmconvert pbf/COD.pbf -o=cod.o5m
+#The replication files are organized according to the url scheme:
+#http://planet.openstreetmap.org/replication/[day|hour|minute]/AAA/BBB/CCC.osc.gz
+#The AAA/BBB/CCC are equivalent to a sequence number, AAA*1000000 + BBB*1000 + CCC
+#The latest osm update file is stored with this sequence number in the filename
+#This allows you determine the file to update in the sequence
 
-# Filter to highways
-osmfilter cod.o5m --keep="highway=*" --out-o5m > codh.o5m
+
+for var in "$@"
+do
+    echo "processing ""$var"
+
+    unset -v latest
+    unset -v prefix
+    prefix=current/"$var"_
+
+    # find the most recently updated country o5m file for country
+    for file in "$prefix"*; do
+        [[ $file -nt $latest ]] && latest=$file
+    done
+
+    echo $latest
+
+    current_highway_file=$latest
+
+    # get the seq number
+    current_seq_str=${latest#$prefix}
+    current_seq_no=${current_seq_str%".o5m"}
+    echo "current: " $current_seq_no
+    next_seq_no=$((10#$current_seq_no + 1))
+    echo $next_seq_no
+    lpad_seq=$(printf "%0*d" 9 $next_seq_no)
 
 
+    temp_updated_file=/tmp/"$var"_updated.o5m
+    updated_highway_file=current/"$var"_"$lpad_seq".o5m
+    polygon_file=poly/ne_"$var".poly
 
-# Loop
-# Apply a changefile 
-osmconvert codh.o5m test_updates/*.osc.gz -B=poly/ne_COD.poly -o=codn.o5m
+    #Convert to o5m should already be done
+    #osmconvert pbf/"$var".pbf -o="$var".o5m
 
-# figure out filenames
-osmfilter cod.o5m --keep="highway=*" --out-o5m > codh.o5m
+    # Filter to highways
+    #osmfilter "$var".o5m --keep="highway=*" --out-o5m > $highway_file
 
+    # Apply a changefile 
+    osmconvert "$current_highway_file" minute_replication/"$lpad_seq".osc.gz -B=$polygon_file -o=$temp_updated_file
+
+    osmfilter $temp_updated_file --keep="highway=*" --out-o5m > $updated_highway_file 
+
+done
